4858778128a7cad2410de7b677bfb3887198a9b6
Siddharth Kumar <siddarth306@gmail.com>
2018-10-19 10:19:25 -0700

    Updated Skill progress bar levels

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ if($().circleProgress){
 
 	//Set progress circle 1
 	$("#progress1").circleProgress({
-		value: 0.75,
+		value: 0.85,
 		size: 175,
 		thickness: 2,
 		fill: "#40424a",
@@ -92,7 +92,7 @@ if($().circleProgress){
 	});
 	//Set progress circle 2
 	$("#progress2").circleProgress({
-		value: 0.83,
+		value: 0.70,
 		size: 175,
 		thickness: 2,
 		fill: "#40424a",
